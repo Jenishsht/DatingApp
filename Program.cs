ca26a21f2a83ba130420fedbdb6ce138581fad7b
Jenish Shrestha <jenishshrestha@Jenishs-MacBook-Air.local>
2024-12-06 08:33:06 +0545

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(opt =>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
